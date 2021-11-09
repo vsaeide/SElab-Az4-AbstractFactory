e089773dba3249aec6987a6e7f6d5b58c9f558b7
vsaeide <vsaeide7798@gmail.com>
2021-11-09 23:57:40 +0330

    Client class implemented

---
+++ src/main/Client.java
@@ -0,0 +1,40 @@
+package main;
+
+
+public class Client {
+
+    private GardenFactory gardenfactory;
+    private Tree tree;
+    private Flower flower;
+
+    public Client(GardenFactory gardenfactory) {
+
+        this.gardenfactory = gardenfactory;
+        CreatGarden();
+    }
+
+    private void CreatGarden() {
+        this.tree = gardenfactory.CreatTree();
+        this.flower = gardenfactory.CreatFlower();
+        System.out.println("tree and garden had to crated");
+
+    }
+
+    @Override
+    public String toString() {
+        String output="The Garden is "+ gardenfactory.type.toString();
+        output+= "\nThe Tree is "+tree.type;
+        output+="\nThe flower is "+flower.type;
+
+        return output;
+    }
+
+
+    public Tree getTree() {
+        return tree;
+    }
+
+    public Flower getFlower() {
+        return flower;
+    }
+}
