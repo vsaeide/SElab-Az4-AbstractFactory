d8ff4f1c34f46661c0c6d9541ac8693418f5aa31
vsaeide <vsaeide7798@gmail.com>
2021-11-09 23:47:10 +0330

    iranian garden type test

---
+++ src/test/IranianGardenTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import main.Client;
+import main.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+class IranianGardenTest {
+
+
+    private GardenFactory gardenFactory = new IranianGarden();
+
+    private Client client = new Client(gardenFactory);
+
+    @Test
+    void GardenType() {
+
+        Assertions.assertEquals(gardenFactory.getType(), Type.Iranian);
+
+    }
+
+
+}
